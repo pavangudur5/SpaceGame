43755a3e6d6738afc141e126519cdbbbe46c5b68
pavangudur <pavankumarga@gmail.com>
2018-05-17 21:21:09 -0400

    Added score

--- SpaceGame/GameScene.swift
+++ SpaceGame/GameScene.swift
@@ -9,25 +9,25 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var starfield:SKEmitterNode!
     var player:SKSpriteNode!
     
-    var scoreLabel : SKLabelNode!
+    var scoreLabel:SKLabelNode!
     var score:Int = 0 {
         didSet {
-            scoreLabel.text = "Score \(score)"
+            scoreLabel.text = "Score: \(score)"
         }
     }
     
     override func didMove(to view: SKView) {
-       
-        starfield = SKEmitterNode(fileNamed: "Starfield.sks");
+        
+        starfield = SKEmitterNode(fileNamed: "Starfield")
         starfield.position = CGPoint(x: 0, y: 1472)
         starfield.advanceSimulationTime(10)
         self.addChild(starfield)
-
+        
         starfield.zPosition = -1
         
         player = SKSpriteNode(imageNamed: "shuttle")
@@ -37,9 +37,16 @@ class GameScene: SKScene {
         self.addChild(player)
         
         self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
-        self.physicsWorld.contactDelegate = self as? SKPhysicsContactDelegate
+        self.physicsWorld.contactDelegate = self
         
         scoreLabel = SKLabelNode(text: "Score: 0")
+        scoreLabel.position = CGPoint(x: 100, y: self.frame.size.height - 60)
+        scoreLabel.fontName = "AmericanTypewriter-Bold"
+        scoreLabel.fontSize = 36
+        scoreLabel.fontColor = UIColor.white
+        score = 0
+        
+        self.addChild(scoreLabel)
     }
     
     override func update(_ currentTime: TimeInterval) {
